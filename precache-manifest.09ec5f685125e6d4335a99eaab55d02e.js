c7c586d0629f16d99c907e8bd0767a6e3abefc1e
Bojan Krstic <krstic.bojan80@gmail.com>
2019-06-09 14:50:21 +0200

    Updates

---
+++ precache-manifest.09ec5f685125e6d4335a99eaab55d02e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ecbe8f18b3205ccabc342c2ddd71c1",
+    "url": "/app-manager/index.html"
+  },
+  {
+    "revision": "176dbc40d24900341bf2",
+    "url": "/app-manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f3a754705bd2a0afa337",
+    "url": "/app-manager/static/css/main.5094970c.chunk.css"
+  },
+  {
+    "revision": "176dbc40d24900341bf2",
+    "url": "/app-manager/static/js/2.3f17d455.chunk.js"
+  },
+  {
+    "revision": "f3a754705bd2a0afa337",
+    "url": "/app-manager/static/js/main.179dcce7.chunk.js"
+  },
+  {
+    "revision": "7f738e55c6c8c3c133a1",
+    "url": "/app-manager/static/js/runtime~main.a4f3a152.js"
+  }
+]);
